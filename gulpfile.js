0c57689a3631acc4403475722c0ac7b604d0b52c
AdamKrukowskiWeb <j.ksiazek@insignia.pl>
2016-09-15 14:00:44 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ var gulp = require('gulp')
     gutil = require('gulp-util');
     sass = require('gulp-sass');
 
+gulp.task('sass', function(){
+    return gulp.src('source/scss/**/*.scss');
+    .pipe(sass())
+    .pipe(gulp.dest('public/assets/stylesheets'));
+});
 
-gulp.watch('source/scss/**/*scss',['sass']);
+gulp.task('sass:watch', function(){
+    gulp.watch('source/scss/**/*scss',['sass']);    
+  
+});
+
 
 
 gulp.task('default', function(){
