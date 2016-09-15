8f304f85874a8927cca741db62e8dfc4faa0e755
AdamKrukowskiWeb <j.ksiazek@insignia.pl>
2016-09-15 12:29:57 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp')
     gutil = require('gulp-util');
+    sass = require('gulp-sass');
 
-    
+
+gulp.watch('source/scss/**/*scss',['sass']);
+
+
 gulp.task('default', function(){
     return gutil.log("gulp smiga");
 });
