a862885b639edba66981b605ad97f60642d46eff
AdamKrukowskiWeb <j.ksiazek@insignia.pl>
2016-09-15 16:55:13 +0200

    zmiana gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,11 @@
 // Load plugins
 var gulp = require('gulp')
-    gutil = require('gulp-util');
     sass = require('gulp-sass');
+    connect = require('gulp-connect');
   
 // Styles 
 var input = './source/scss/**/*.scss';
 var output = './public/assets/css';
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
-};
 
-gulp.task('sass', function(){
-    return gulp
-    .src(input)
-    .pipe(sass(sassOptions).on('error', sass.logError))   
-    .pipe(gulp.dest(output));
-});
 
-gulp.task('watch', function(){
-    gulp.watch(input, ['sass']);
-});
-
-gulp.task('default', ['sass'], function() {
-  return gutil.log('Gulp is running!')
-});
+//https://css-tricks.com/gulp-for-beginners/
